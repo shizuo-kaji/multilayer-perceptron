4f7578996e9d6342ffa65376875cc6e292f1b29b
shizuo-kaji <shizuo.kaji@gmail.com>
2018-03-21 21:56:15 +0900

    fixed bugs

--- MLP.py
+++ MLP.py
@@ -34,38 +34,32 @@ activ = {
 class MLP(chainer.Chain):
     def __init__(self, args):
         super(MLP, self).__init__()
+        initializer = chainer.initializers.HeNormal()
         self.activ=activ[args.activation]
-        self.layers = args.layers
-        self.out_ch = args.out_ch
         self.dropout_ratio = args.dropout_ratio
-        self.l = []
-        # with self.init_scope():
-        #     self.l.append(L.Linear(None,args.unit))
-        #     for i in range(1,self.layers):
-        #         self.l.append(L.Linear(args.unit,args.unit))
-        #     self.l.append(L.Linear(args.unit,args.out_ch))            
-        self.add_link('layer{}'.format(0), L.Linear(None,args.unit))
-        for i in range(1,self.layers):
-            self.add_link('layer{}'.format(i), L.Linear(args.unit,args.unit))
-        self.add_link('fin_layer', L.Linear(args.unit,args.out_ch))
+        self.unit = args.unit
+        self.batchnorm = args.batchnorm
+        self.regression = args.regression
+        for i in range(len(self.unit)):
+            self.add_link('layer{}'.format(i), L.Linear(None,args.unit[i],initialW=initializer))
+            if args.batchnorm:
+                self.add_link('bnlayer{}'.format(i), L.BatchNormalization(args.unit[i]))
 
     def __call__(self, x, t):
-        # h = F.dropout(self.activ(self.l[0](x)),ratio=self.dropout_ratio)
-        # for i in range(1,self.layers):
-        #     h = F.dropout(self.activ(self.l[i](h)),ratio=self.dropout_ratio)
-        # h = self.l[-1](h)
-        
-        h = F.dropout(self.activ(self['layer{}'.format(0)](x)),ratio=self.dropout_ratio)
-        for i in range(1,self.layers):
-            h = F.dropout(self.activ(self['layer{}'.format(i)](h)),ratio=self.dropout_ratio)
-        h = self['fin_layer'](h)
-
-        if self.out_ch > 1:    # classification
-            loss = F.softmax_cross_entropy(h, t)
-            chainer.report({'loss': loss, 'accuracy': F.accuracy(h, t)}, self)
-        else:   #regression
+        h = x
+        for i in range(len(self.unit)-1):
+            h = self['layer{}'.format(i)](h)
+            if self.batchnorm:
+                h = self['bnlayer{}'.format(i)](h)
+            h = self.activ(h)
+            h = F.dropout(h,ratio=self.dropout_ratio)
+        h = self['layer{}'.format(len(self.unit)-1)](h)
+        if self.regression:
             loss = F.mean_squared_error(t, h)
             chainer.report({'loss': loss}, self)
+        else:
+            loss = F.softmax_cross_entropy(h, t)
+            chainer.report({'loss': loss, 'accuracy': F.accuracy(h, t)}, self)
         if chainer.config.train:
             return loss
         return h
@@ -85,51 +79,62 @@ def main():
                         help='Number of sweeps over the dataset to train')
     parser.add_argument('--snapshot', '-s', type=int, default=-1,
                         help='snapshot interval')
-    parser.add_argument('--label_index', '-l', type=int, default=0,
-                        help='Column number of the target variable')
-    parser.add_argument('--gpu', '-g', type=int, default=-1,
+    parser.add_argument('--labelcol', '-l', type=int, nargs="*", default=[0,1,2,3],
+                        help='column indices of target variables')
+    parser.add_argument('--initmodel', '-i',
+                        help='Initialize the model from given file')
+    parser.add_argument('--gpu', '-g', type=int,
                         help='GPU ID (negative value indicates CPU)')
     parser.add_argument('--outdir', '-o', default='result',
                         help='Directory to output the result')
-    parser.add_argument('--out_ch', '-oc', type=int, default=1,
-                        help='num of output channels. set to 1 for regression')
     parser.add_argument('--optimizer', '-op', default='MomentumSGD',
                         help='optimizer {MomentumSGD,AdaDelta,AdaGrad,Adam,RMSprop}')
     parser.add_argument('--resume', '-r', default='',
                         help='Resume the training from snapshot')
-    parser.add_argument('--skip_rows', '-sr', type=int, default=1,
+    parser.add_argument('--skip_rows', '-sr', type=int, default=0,
                         help='num of rows skipped in the data')
     parser.add_argument('--skip_column', '-sc', type=int, nargs="*", default=[],
                         help='set of indices of columns to be skipped in the data')
-    parser.add_argument('--layers', '-nl', type=int, default=3,
-                        help='Number of layers')
-    parser.add_argument('--unit', '-nu', type=int, default=100,
+    parser.add_argument('--unit', '-nu', type=int, nargs="*", default=[128,64,32,4],
                         help='Number of units in the hidden layers')
     parser.add_argument('--test_every', '-t', type=int, default=5,
                         help='use one in every ? entries in the dataset for validation')
+    parser.add_argument('--regression', action='store_true',
+                        help="set for regression, otherwise classification")
+    parser.add_argument('--batchnorm','-bn', action='store_true',
+                        help="perform batchnormalization")
     parser.add_argument('--predict', action='store_true')
     parser.add_argument('--weight_decay', '-w', type=float, default=1e-5,
                         help='weight decay for regularization')
     args = parser.parse_args()
 
     ##
+    if not args.gpu:
+        if chainer.cuda.available:
+            args.gpu = 0
+        else:
+            args.gpu = -1          
+
     print('GPU: {} Minibatch-size: {} # epoch: {}'.format(args.gpu,args.batchsize,args.epoch))
 
     # Set up a neural network to train
     model = MLP(args)
+    if args.initmodel:
+        print('Load model from: ', args.initmodel)
+        chainer.serializers.load_npz(args.initmodel, model)
     if args.gpu >= 0:
         chainer.cuda.get_device(args.gpu).use()  # Make a specified GPU current
         model.to_gpu()  # Copy the model to the GPU
 
-    # Setup an optimiser
+    # Set up an optimizer
     if args.optimizer == 'MomentumSGD':
-        optimizer = chainer.optimizers.MomentumSGD(lr=0.003, momentum=0.9)
+        optimizer = chainer.optimizers.MomentumSGD(lr=0.01, momentum=0.9)
     elif args.optimizer == 'AdaDelta':
         optimizer = chainer.optimizers.AdaDelta(rho=0.95, eps=1e-06)
     elif args.optimizer == 'AdaGrad':
-        optimizer = chainer.optimizers.AdaGrad(lr=0.001, eps=1e-08)
+        optimizer = chainer.optimizers.AdaGrad(lr=0.01, eps=1e-08)
     elif args.optimizer == 'Adam':
-        optimizer = chainer.optimizers.Adam(alpha=0.001, beta1=0.9, beta2=0.999, eps=1e-08)
+        optimizer = chainer.optimizers.Adam(alpha=0.01, beta1=0.9, beta2=0.999, eps=1e-08)
     elif args.optimizer == 'RMSprop':
         optimizer = chainer.optimizers.RMSprop(lr=0.01, alpha=0.99, eps=1e-08)
     else:
@@ -138,34 +143,32 @@ def main():
     optimizer.setup(model)
     if args.weight_decay>0:
         optimizer.add_hook(chainer.optimizer.WeightDecay(args.weight_decay))
-    print('layers: {}, units: {}, optimiser: {}, Weight decay: {}, dropout ratio: {}'.format(args.layers,args.unit,args.optimizer,args.weight_decay,args.dropout_ratio))
+    print('units: {}, optimiser: {}, Weight decay: {}, dropout ratio: {}'.format(args.unit,args.optimizer,args.weight_decay,args.dropout_ratio))
 
     # select numpy or cupy
     xp = chainer.cuda.cupy if args.gpu >= 0 else np
-    label_type = np.int32 if args.out_ch>1 else np.float32
+    label_type = np.float32 if args.regression else np.int32
 
     # read csv file
     csvdata = np.loadtxt(args.dataset, delimiter=",", skiprows=args.skip_rows)
     ind = np.ones(csvdata.shape[1], dtype=bool)  # indices for unused columns
-    ind[args.label_index] = False
+    ind[args.labelcol] = False
     for i in args.skip_column:
         ind[i] = False
-    x = csvdata[:,ind]
-    t = csvdata[:,args.label_index][:,np.newaxis]
-    print('target column: {}, excluded columns: {}'.format(args.label_index,np.where(ind==False)[0].tolist()))
+    x = np.array(csvdata[:,ind],dtype=np.float32)
+    t = csvdata[:,args.labelcol]
+    t = np.array(t, dtype=label_type)
+    if not args.regression:
+        t = t[:,0]
+    print('target column: {}, excluded columns: {}'.format(args.labelcol,np.where(ind==False)[0].tolist()))
     print("variable shape: {}, label shape: {}, label type: {}".format(x.shape, t.shape, label_type))
-    x = np.array(x, dtype=np.float32)
-    if args.out_ch > 1:
-        t = np.array(np.ndarray.flatten(t), dtype=label_type)
-    else:
-        t = np.array(t, dtype=label_type)
 
 ## train-validation data
 # random spliting
     #train, test = datasets.split_dataset_random(datasets.TupleDataset(x, t), int(0.8*t.size))
 # splitting by modulus of index
-    train_idx = [i for i in range(t.size) if (i+1) % args.test_every != 0]
-    var_idx = [i for i in range(t.size) if (i+1) % args.test_every == 0]
+    train_idx = [i for i in range(len(t)) if (i+1) % args.test_every != 0]
+    var_idx = [i for i in range(len(t)) if (i+1) % args.test_every == 0]
     n = len(train_idx)
     train_idx.extend(var_idx)
     train, test = datasets.split_dataset(datasets.TupleDataset(x, t), n, train_idx)
@@ -181,10 +184,15 @@ def main():
     log_interval = 1, 'epoch'
     val_interval = frequency/10, 'epoch'
 
-    trainer.extend(extensions.snapshot(), trigger=(frequency, 'epoch'))
+    trainer.extend(extensions.snapshot(filename='snapshot_epoch_{.updater.epoch}'), trigger=(frequency, 'epoch'))
+    trainer.extend(extensions.snapshot_object(
+            model, 'model_epoch_{.updater.epoch}'), trigger=(frequency/5, 'epoch'))
     trainer.extend(extensions.LogReport(trigger=log_interval))
-    trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu),trigger=val_interval)
     trainer.extend(extensions.dump_graph('main/loss'))
+
+    if args.optimizer in ['MomentumSGD','AdaGrad','RMSprop']:
+        trainer.extend(extensions.observe_lr(), trigger=log_interval)
+        trainer.extend(extensions.ExponentialShift('lr', 0.5), trigger=(args.epoch/5, 'epoch'))
 
     if extensions.PlotReport.available():
         trainer.extend(
@@ -195,9 +203,10 @@ def main():
                 ['main/accuracy', 'validation/main/accuracy'],
                 'epoch', file_name='accuracy.png'))
 
-    trainer.extend(extensions.PrintReport(
-        ['epoch', 'main/loss', 'validation/main/loss',
-         'main/accuracy', 'validation/main/accuracy', 'elapsed_time']), trigger=log_interval)
+    trainer.extend(extensions.PrintReport([
+            'epoch', 'main/loss', 'main/accuracy', 'validation/main/loss',
+         'validation/main/accuracy', 'elapsed_time', 'lr'
+         ]),trigger=log_interval)
 
     trainer.extend(extensions.ProgressBar(update_interval=10))
 
@@ -222,23 +231,25 @@ def main():
         y = model(x,t)
     if args.gpu >= 0:
         pred = chainer.cuda.to_cpu(y.data)
+        t = chainer.cuda.to_cpu(t)
     else:
         pred = y.data
-    if args.out_ch > 1:    # classification
+    if args.regression:
+        left = np.arange(t.shape[0])
+        for i in range(len(args.labelcol)):
+            rmse = F.mean_squared_error(pred[:,i],t[:,i])
+            plt.plot(left, t[:,i], color="royalblue")
+            plt.plot(left, pred[:,i], color="crimson", linestyle="dashed")
+            plt.title("RMSE: {}".format(np.sqrt(rmse.data)))
+            plt.savefig(args.outdir+'/result{}.png'.format(i))
+            plt.close()
+        result = np.hstack((t,pred))
+        np.savetxt(args.outdir+"/result.csv", result , fmt='%1.5f', delimiter=",", header="truth,prediction")
+    else:
         p=np.argmax(pred,axis=1)
         result = np.vstack((t,p)).astype(np.int32).transpose()
         print(result.tolist())
         np.savetxt(args.outdir+"/result.csv", result, delimiter="," ,header="truth,prediction")
-    else:
-        rmse = F.mean_squared_error(pred,t)
-        result = np.vstack((t,pred)).transpose()
-        np.savetxt(args.outdir+"/result.csv", result , delimiter="," ,header="truth,prediction")
-        # draw a graph
-        left = np.arange(len(test))
-        plt.plot(left, t, color="royalblue")
-        plt.plot(left, pred, color="crimson", linestyle="dashed")
-        plt.title("RMSE: {}".format(np.sqrt(rmse.data)))
-        plt.savefig(args.outdir+'/result.png')
 
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -28,28 +28,26 @@ does the job. It converts the downloaded file iris.data.txt into a csv compatibl
 `python MLP.py -h`
 gives a brief description of command line arguments
 
-`python MLP.py iris.csv -l 0 -sr 0 -dr 0 -nu 80 -nl 3 -oc 3 -t 5 -a relu`
-reads iris.csv and use every 5 entries for validation and the rest for training.
-Then, it predicts the value of the 0th column (class)
-from other columns using a neural network with 80 units times 5 hidden layers,
-and with ReLU as an activation function.
-Note that there are three classes in the dataset so -oc 3.
-The result is recorded in the file result/nn-out.csv.
+`python MLP.py iris.csv -l 0 -sr 0 -nu 80 80 80 3 -t 5 -a relu -bn`
+reads iris.csv and use every 5 entries (-t 5) for validation and the rest for training.
+Then, it predicts the value of the 0th column (-l 0, 3 classes)
+from other columns using a neural network with 80 80 80 3 units (-nu 80 80 80 3)
+and with ReLU as an activation function )-a relu) using Batchnormalization (-bn).
+Notice that the number of units in the last layer agrees with the number of the classes.
+The result is recorded in the file result/result.csv.
 
-`python MLP.py iris.csv -l 0 -sr 0 -dr 0 -nu 80 -nl 3 -oc 3 -t 5 -a relu -g 0`
-same as above but with GPU (working installation of cupy with CUDA support is required).
-
-`python MLP.py iris.csv -l 0 -sr 0 -dr 0 -nu 80 -nl 3 -oc 3 --predict -r result/snapshot_iter_1200`
-does not perform training but instead read the trained snapshot from result/snapshot_iter_1200
-and predicts all entries from iris.csv.
-
-`python MLP.py iris.csv -l 1 -sr 0 -sc 0 -dr 0 -nu 80 -nl 3 -oc 1 -t 6`
+`python MLP.py iris.csv -l 1 -sr 0 -sc 0 -dr 0 -nu 80 80 1 -t 6 --regression`
 reads iris.csv and use every 6 entries for validation and the rest for training.
 Then, it predicts the value of the 1st column (sepal length)
-from columns except for 0th (specified by -sc) and 1st.
+from columns except for 0th (-sc 0) and 1st.
 It plots a graph with matplotlib.
 The column indices excluded from training and prediction are specified with -sc
 (e.g., -sc 1 3 4 8).
+
+`python MLP.py iris.csv -l 1 2 -sr 0 -dr 0.3 -nu 80 80 2 -t 6 --regression`
+does multi-column (-l 1 2) regression at a time.
+Notice that the number of target columns should match the number of units in the last layer (in this case 2).
+
 
 # LIMITATION
 CSV files should not contain non-numerical values.
